231255568e75b09341d03ba29e9ccaa7952b0d9d
Sasskky <nickybro2005@gmail.com>
2022-09-26 11:43:08 -0600

    fixed

---
+++ image/sketch.js
@@ -0,0 +1,30 @@
+// Image proccesing
+// Nick Brophy
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let fishImage;
+let scalee = 1.5;
+
+function preload() {
+  fishImage = loadImage("fish.jpg");
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW){
+  scalee = scalee * 1.5;
+}
+else if (keyCode === DOWN_ARROW); {
+scalee = scalee * 1.5;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+}
+}
+function draw() {
+  background(220);
+  image(fishImage, mouseX, mouseY, fishImage.width*scalee, fishImage.height*scalee);
+}
