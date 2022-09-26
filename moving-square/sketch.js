02c49e5048b0c7ad95fe110aaa83eedc3f50a1bc
Sasskky <nickybro2005@gmail.com>
2022-09-26 12:07:44 -0600

    Update sketch.js
    uodated code

--- moving-square/sketch.js
+++ moving-square/sketch.js
@@ -1,21 +1,37 @@
 // square around edge
 // Brophy
 // 09/26/22
-
+let squareLocation; 
+let x = 0;
+let y = 0;
+let squareSize;
+let speed;
+let state;
 
 function setup() {
   createCanvas(400, 400);
+  squareSize = 60;
+  speed = 5;
+  state = "right";
 }
 
 function draw() {
   background("white");
+  moveSquare();
   drawSquare();
 }
 
 function drawSquare() {
   strokeWeight(4);
   stroke("black");
-  square(30,20,55); 
+  square(x,y,squareSize); 
 }
 
+function moveSquare() {
+  if (state === "right") {
+    x += speed;
+    if (x - squareSize >= width) {
+  }
+  }
+}
 
