4720d93a7be137b9ab06c515cc6a6e6ab54d376d
Sasskky <nickybro2005@gmail.com>
2022-10-04 12:07:20 -0600

    hitbox demo

--- hit detector/sketch.js
+++ hit detector/sketch.js
@@ -1,15 +1,31 @@
 // Project Title
-// Your Name
+// Brophy
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let hit = false;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  circle(200, 200, 100);
+  strokeWeight(4);
+  stroke("black");
+  point(mouseX, mouseY);
+
+  hit = collidePointCircle(mouseX, mouseY, 200, 200, 100);
+
+  // Use vectors as input:
+  // const mouse  = createVector(mouseX, mouseY);
+  // const circle = createVector(200, 200);
+  // const diam   = 100;
+  // hit = collidePointCircleVector(mouse, circle, diam);
+  strokeWeight(4);
+  stroke(hit ? color("red") : 0);
+  print("colliding?", hit);
 }
