ecf26dd6da8a753ade6ad3cb4e433f6f8b5b6eb9
Sasskky <nickybro2005@gmail.com>
2022-10-04 11:54:07 -0600

    start of first project
    worked on first project

---
+++ firstproject/sketch.js
@@ -0,0 +1,24 @@
+// First Project
+// Brophy
+// 10/4/22
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let backGround;
+
+function preLoad() {
+  backGround = loadImage("snakebackground.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  image = 
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(220);
+  preLoad;
+}
+
