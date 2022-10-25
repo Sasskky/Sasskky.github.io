1033f5d601f1c56283058702c20ec887ef952cd5
Sasskky <nickybro2005@gmail.com>
2022-10-25 12:01:03 -0600

    2D Grid Demo
    made a 2d grid

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,9 +1,10 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Basic Grid Demo
+// Brophy
+// 10/25/22
+
+let grid = [[1, 0, 1],
+            [1, 0, 1],
+            [0, 1, 0]];
 
 
 function setup() {
@@ -12,4 +13,21 @@ function setup() {
 
 function draw() {
   background(220);
+  displayGrid(grid);
+}
+
+function displayGrid(grid) {
+  let cellWidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+  for (let y = 0; y<grid.length; y++) {
+    for (let x = 0; x<grid[y].length; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[x][y] === 1) {
+        fill ("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
 }
