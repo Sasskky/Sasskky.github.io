045e8b8d22ba50e1f4d10e90e794fc0e887970d3
Sasskky <nickybro2005@gmail.com>
2022-11-08 11:40:39 -0600

    created file for project

---
+++ Brophy-2d-Arrary-Project/sketch.js
@@ -0,0 +1,15 @@
+// 2d Array Project
+// Brophy
+// 11/8/22
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
