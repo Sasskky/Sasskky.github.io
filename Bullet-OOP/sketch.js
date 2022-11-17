23d01dd411c58d230f7c6b084a4d4cb0c2742ef4
Sasskky <nickybro2005@gmail.com>
2022-11-17 11:51:10 -0600

    Bullet Demo

--- Bullet-OOP/sketch.js
+++ Bullet-OOP/sketch.js
@@ -3,13 +3,34 @@
 // 11/16/2022
 
 class Bullet {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+  constructor(theImage) {
+    this.x = 50;
+    this.y = 200;
     this.radius = 2;
-    this.color = "White";
-    this.dx += 5;
+    this.dx = 5;
+    this.image = someIMG;
   }
+
+  display () {
+    image(this.image, this.x, this.y, this.image.width*0.3, this.image.height*0.3);
+   
+    // fill("White");
+    // circle(this.x, this.y, this.radius*2);
+  }
+  move() {
+    this.x += this.dx;
+  }
+
+  isDead() {
+    return this.x >= width;
+  }
+}
+
+let bullets = [];
+let someIMG;
+
+function preload() {
+  someIMG = loadImage("Bullet-Bill.jpg");
 }
 
 function createMan() {
@@ -27,4 +48,20 @@ function setup() {
 function draw() {
   background("black");
   createMan();
+  for (let someBullet of bullets) {
+    someBullet.move();
+    someBullet.display();
+  }
+
+  // delete
+  for (let i = bullets.length-1; i >= 0; i--) {
+    if (bullets[i].isDead()) {
+      bullets.splice(i, 1);
+    }
+  }
+}
+
+function mousePressed() {
+  let someBullet = new Bullet(someIMG);
+  bullets.push(someBullet);
 }
